6be826c3981f55a99ddf5b230c5ae028da82491c
junghee-dev <>
2024-10-09 18:08:17 +0900

    템플릿 페이지 생성 및 DB세팅

---
+++ src/main/java/com/example/heelog/entity/User.java
@@ -0,0 +1,8 @@
+package com.example.heelog.dto;
+
+public class User {
+
+    String id;
+    String name;
+    String email;
+}
--- src/main/java/com/example/heelog/service/UserService.java
+++ src/main/java/com/example/heelog/service/UserService.java
@@ -1,4 +1,9 @@
 package com.example.heelog.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class HeelogService {
+
+
 }
